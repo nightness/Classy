ffc997a8fa2d7a07bf747ac685a8bba31e7155bc
nightness <joshguyette@yahoo.com>
2024-08-20 11:51:57 -0500

    refactoring and comments

--- Observable.lua
+++ Observable.lua
@@ -1,43 +1,58 @@
 Classy.Observable = {
-    new = function (initialValue)
-        return Classy.createInstance(Classy.Observable.prototype, function (self)
-            self._value = initialValue;
-        end);
+    -- Creates a new Observable instance with an initial value
+    new = function(initialValue)
+        return Classy.createInstance(Classy.Observable.prototype, function(self)
+            self._value = initialValue  -- Initialize the observable value
+        end)
     end,
+
     prototype = {
-        constructor = function (self)
-            self._className = "Classy.Observable";
-            self._listeners = Classy.DataSet.new();
+        -- Constructor initializes the Observable with an empty set of listeners
+        constructor = function(self)
+            self._className = "Classy.Observable"
+            self._listeners = Classy.DataSet.new()  -- Store unique listeners
         end,
-        get = function (self) 
-            return self._value;
+
+        -- Returns the current value of the Observable
+        get = function(self)
+            return self._value
         end,
-        set = function (self, value)
-            if (value ~= self._value) then
-                self._value = value;
-                self:invokeValueChanged(value);
+
+        -- Sets a new value for the Observable and notifies listeners if the value has changed
+        set = function(self, value)
+            if value ~= self._value then
+                self._value = value
+                self:invokeValueChanged(value)  -- Notify listeners about the change
             end
         end,
-        subscribe = function (self, func)
-            -- Subscribe to value changed events
-            if (type(func) == "function") then
-                self._listeners:add(func);
+
+        -- Subscribes a function to the Observable's value changes
+        subscribe = function(self, func)
+            if type(func) == "function" then
+                self._listeners:add(func)  -- Add the listener function
             end
         end,
-        unsubscribe = function (self, func)
-            -- Unsubscribe to value changed events
-            self._listeners:remove(func);
+
+        -- Unsubscribes a function from the Observable's value changes
+        unsubscribe = function(self, func)
+            self._listeners:remove(func)  -- Remove the listener function
         end,
-        invokeErrorHandler = function (err)
-            addon.Debug("Observable invoke error: "..tostring(err));
-        end,        
-        invokeValueChanged = function (self, newValue)
-            -- Clones the handler array
-            local listeners = self._listeners:clone();            
-            -- Invoke all functions in _listeners.
-            for key, value in ipairs(listeners) do
-                if (listeners[key]) then
-                    xpcall(listeners[key], self.invokeErrorHandler, newValue)
+
+        -- Default error handler for listener invocation errors
+        invokeErrorHandler = function(err)
+            addon.Debug("Observable invoke error: " .. tostring(err))
+        end,
+
+        -- Invokes all subscribed listeners when the value changes
+        invokeValueChanged = function(self, newValue)
+            -- Copy the listeners to avoid modification during iteration
+            local listeners = self._listeners:clone()
+
+            -- Notify all listeners of the new value
+            for _, listener in ipairs(listeners) do
+                if listener then
+                    -- Call each listener with error handling
+                    xpcall(listener, function(err) self:invokeErrorHandler(err) end, newValue)
                 end
             end
         end,
