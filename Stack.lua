cd4affc14b9da2bc8172501867fe4258f40a79e6
nightness <joshguyette@yahoo.com>
2024-08-20 12:01:59 -0500

    cleaned up this class, removed the UndoStack

--- Stack.lua
+++ Stack.lua
@@ -1,64 +1,50 @@
---
--- Classy's Stack datatype
---
 Classy.Stack = {
-    new = function (initializer)
-        return Classy.createInstance(Classy.Stack.prototype, initializer);
+    -- Creates a new Stack instance
+    new = function(initializer)
+        return Classy.createInstance(Classy.Stack.prototype, initializer)
     end,
+
     prototype = {
+        -- Constructor initializes the Stack with an empty Collection
         constructor = function(self, ...)
-            self._className = "Classy.Stack";
-            self._collection = Classy.Collection.new();
+            self._className = "Classy.Stack"
+            self._collection = Classy.Collection.new()  -- Internal storage for stack elements
         end,
 
-        reset = function (self)
-            self._collection:reset();
+        -- Resets the stack to its initial empty state
+        reset = function(self)
+            self._collection:reset()  -- Clear the underlying collection
         end,
 
+        -- Pushes an element onto the stack
         push = function(self, arg)
-            self._collection:pushleft(arg);
+            self._collection:pushleft(arg)  -- Add element to the top of the stack
         end,
 
+        -- Pops and returns the top element from the stack
         pop = function(self)
-            return self._collection:popleft();
+            if self:isEmpty() then
+                error("Classy.Stack: pop from an empty stack")  -- Error handling for empty stack
+            end
+            return self._collection:popleft()  -- Remove element from the top of the stack
+        end,
+
+        -- Peeks at the top element of the stack without removing it
+        peek = function(self)
+            if self:isEmpty() then
+                return nil  -- Return nil if the stack is empty
+            end
+            return self._collection:get(self._collection.first)  -- Return the top element
+        end,
+
+        -- Checks if the stack is empty
+        isEmpty = function(self)
+            return self._collection:isEmpty()  -- Returns true if the stack is empty
+        end,
+
+        -- Returns the number of elements in the stack
+        size = function(self)
+            return self._collection.last - self._collection.first + 1  -- Calculate the size of the stack
         end,
     },
-}
-
---
--- A stack that invokes callbacks, sutabile to undo actions. This stack will invoke callbacks on undo() calls.
---
-Classy.UndoStack = {
-    new = function (initializer)
-        return Classy.createInstance(Classy.UndoStack, initializer);
-    end,
-
-    constructor = function(self, ...)
-        self._className = "Classy.UndoStack";
-        self._collection = Classy.Collection.new()
-    end,
-
-    reset = function (self)
-        self._collection:reset();
-    end,
-
-    add = function(self, callback)
-        self._collection:pushleft(callback)
-    end,
-
-    undo = function(self)
-        if (not self._collection:isEmpty()) then
-            local callback = self._collection:popleft()
-            -- Callback is a function
-            if (type(callback) == "function") then
-                callback()
-                return true;
-            -- Callback is a Job (or is a table that has an invoke() function)
-            elseif (type(callback) == "table" and type(callback.invoke) == "function") then
-                callback.invoke(); 
-                return true;
-            end
-        end
-        return false
-    end,
 }
