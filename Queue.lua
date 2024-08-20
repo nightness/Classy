018b66b3bf5173c9b94757db3234e17602fc17e3
nightness <joshguyette@yahoo.com>
2024-08-20 11:55:02 -0500

    finished this class

--- Queue.lua
+++ Queue.lua
@@ -1,19 +1,45 @@
 Classy.Queue = {
-    new = function (initializer)
-        return Classy.createInstance(Classy.Queue.prototype, initializer);
+    -- Creates a new Queue instance
+    new = function(initializer)
+        return Classy.createInstance(Classy.Queue.prototype, initializer)
     end,
+
     prototype = {
+        -- Constructor initializes the Queue with an empty Collection
         constructor = function(self, ...)
-            self._className = "Classy.Queue";
-            self._collection = Classy.Collection.new();
+            self._className = "Classy.Queue"
+            self._collection = Classy.Collection.new()  -- Internal storage for queue elements
         end,
 
-        add = function(self, arg)
-            self._collection:pushleft(arg);
+        -- Adds an element to the queue (enqueue)
+        enqueue = function(self, arg)
+            self._collection:pushleft(arg)  -- Add element to the front of the queue
         end,
 
-        next = function(self)
-            return self._collection:popright();
+        -- Removes and returns the next element from the queue (dequeue)
+        dequeue = function(self)
+            if self:isEmpty() then
+                error("Classy.Queue: dequeue from an empty queue")  -- Error handling for empty queue
+            end
+            return self._collection:popright()  -- Remove element from the back of the queue
+        end,
+
+        -- Checks if the queue is empty
+        isEmpty = function(self)
+            return self._collection:isEmpty()  -- Returns true if the queue is empty
+        end,
+
+        -- Returns the number of elements in the queue
+        size = function(self)
+            return self._collection.last - self._collection.first + 1  -- Calculate the size of the queue
+        end,
+
+        -- Peeks at the next element in the queue without removing it
+        peek = function(self)
+            if self:isEmpty() then
+                return nil  -- Return nil if the queue is empty
+            end
+            return self._collection:get(self._collection.first)  -- Return the element at the back of the queue
         end,
     },
 }
